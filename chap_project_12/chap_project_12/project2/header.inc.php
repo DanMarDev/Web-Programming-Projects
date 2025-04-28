a8dcfc84876faa602d6bf25885a3b5e816a3fd0c
Daniel Marinelli <daniel.marinelli@scranton.edu>
2025-04-28 18:44:22 -0400

    Small change to project 2

--- chap_project_12/chap_project_12/project2/header.inc.php
+++ chap_project_12/chap_project_12/project2/header.inc.php
@@ -23,7 +23,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Share Your Travels</a>
+                    <a class="navbar-brand" href="list.php">Share Your Travels</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
